a373ec7d25bd8ae44cb5b1034418e221194aa438
Lidya Tsegay <lidyatsegay@outlook.com>
2019-09-09 14:35:52 -0400

    initial commit

---
+++ FunctionToTestConsole/ForcastingFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionToTestConsole {
+    public class ForcastingFunction {
+
+        public int Multiplier { get; set; } = 0;
+
+        public int Forecast(int x, int y) {
+            if (x < 10 || x > -10 || y < 10 || y < -10) {
+                throw new Exception("X or Y are outside the Domain");
+                    }
+            return ((x * x * x) + (y * y)) * Multiplier;
+        }
+    }
+}
--- FunctionToTestConsole/Program.cs
+++ FunctionToTestConsole/Program.cs
@@ -2,8 +2,19 @@
 
 namespace FunctionToTestConsole {
     class Program {
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var ff = new ForcastingFunction();
+            try {
+                 // Multiplier is zero
+                 var result = ff.Forecast(-11, 11); //expect Exception
+                Console.WriteLine("Test Failed!");
+            } catch (Exception ex) {
+                Console.WriteLine("Test Passed!");
+            }
+
+
         }
     }
 }
